a515e434bbb4c9b6bde7bde22d211fa4826f6229
Ahmed Soliman <AhmedSoliman_1234@hotmail.com>
2024-01-09 00:43:01 -0800

    first commit

---
+++ lib/WIFI.cpp
@@ -0,0 +1,3 @@
+void WIFI_Init(){
+    
+}
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "calc.hpp"
+#include "WIFI.hpp"
+#include "defaultconfig.h"
+
+int main()
+{
+    std::cout << sum(1,2);
+    return 0;
+}
